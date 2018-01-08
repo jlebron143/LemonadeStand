e29df43d028533fe3fbcc313690e6bd52d9f3941
jlebron143 <jlebron143@gmail.com>
2018-01-08 14:46:37 -0600

    saving changes to customers

--- Customers.cs
+++ Customers.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace LemonadeStand
 {
-    class Customers
+   public class Customers
     {
+        double measurePercentage = 100;
+        public double chanceOfMaxPurchase;
+        double probabilityOfPurchase;
+        public bool purchase;
+        public int amountOfCupsBought;
+        double temperaturePrediction;
+        double statusPrediction;
+        Random rnd;
+
+        public Customers( double ChanceOfMaxPurchase, double AmountOfCupsBought)
+        {
+            chanceOfMaxPurchase = ChanceOfMaxPurchase;
+            amountOfCupsBought = AmountOfCupsBought;
+
+        }
+
+        public void purchaseTemperatureProbablitity (Forecast forecast)
+        {
+            if( forecast.temperature <= 65)
+            { }
+        }
+
+
+          
+
     }
 }
